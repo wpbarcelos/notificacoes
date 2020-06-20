465e7fc0d565cf9f5c81ec73fc213d652fcd7646
Wanderson Passos Barcelos <wp.barcelos@gmail.com>
2020-06-20 19:12:08 -0300

    adicionado exemplo de curl em php

--- exemplo/php_curl.php
+++ exemplo/php_curl.php
@@ -3,19 +3,24 @@
 $ch = curl_init();
 
 $params = array(
-    'client_id' => 123,
+    // 'client_id' => 123,
+    'dashboard_id' => 123,
     'title' => 'O pedido saiu para entrega',
-    'message' => 'mensagem do php',
-    'type' => 'success', // success | info | danger | warning
+    'message' => 'Novo pedido cadastrado',
+    'type' => 'info', // success | info | warning
+);
 
+$token = 'minhasenha';
+$credentials = array(
+    "authorization: $token"
 );
 curl_setopt($ch, CURLOPT_URL, 'http://localhost:3000/send');
-
 curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $credentials);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-// Receive server response ...
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 
 $server_output = curl_exec($ch);
 
